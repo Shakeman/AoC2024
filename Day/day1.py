4284d019b2b3ac3480f6ffc0bb106574127b376d
John Doe <johndoe@example.com>
2024-12-02 06:30:39 -0700

    Finished Part 2

--- Day/day1.py
+++ Day/day1.py
@@ -12,16 +12,29 @@ def read_input(file_path: Path) -> tuple[list[int], list[int]]:
             right_list.append(right)
     return left_list, right_list
 
-def day_1_process(left_list: list[int], right_list: list[int]):
+def day_1_part_1(left_list: list[int], right_list: list[int]) -> int:
     sorted_left_list: list[int] = sorted(left_list)
     sorted_right_list: list[int] = sorted(right_list)
     differences: list[int] = calculate_differences(sorted_left_list, sorted_right_list)
     return sum(differences)
 
+def day_1_part_2(left_list: list[int], right_list: list[int]) -> int:
+    sorted_left_list: list[int] = sorted(left_list)
+    sorted_right_list: list[int] = sorted(right_list)
+    counts: list[int] = count_occurrences(sorted_left_list, sorted_right_list)
+    similarity: list[int] = multiply_lists(counts, sorted_left_list)
+    return sum(similarity)
 
 def calculate_differences(list1: list[int], list2: list[int]) -> list[int]:
     return [abs(a - b) for a, b in zip(list1, list2)]
 
+def count_occurrences(left_list: list[int], right_list: list[int]) -> list[int]:
+    return [right_list.count(item) for item in left_list]
+
+def multiply_lists(list1: list[int], list2: list[int]) -> list[int]:
+    return [a * b for a, b in zip(list1, list2)]
+
 file_path: Path = Path.cwd() / "Input" / "day1.txt"
 left_list, right_list = read_input(file_path)
-print(day_1_process(left_list, right_list))
+print(day_1_part_1(left_list, right_list))
+print(day_1_part_2(left_list, right_list))
