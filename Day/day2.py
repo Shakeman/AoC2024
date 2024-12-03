560c5bed70a85bfa2d168dca2a90b06bf629207e
John Doe <johndoe@example.com>
2024-12-03 06:49:02 -0700

    Ruff formatting

--- Day/day2.py
+++ Day/day2.py
@@ -1,6 +1,5 @@
 import itertools
 from pathlib import Path
-import re
 
 
 def read_input(file_path: Path) -> list[list[int]]:
@@ -11,19 +10,23 @@ def read_input(file_path: Path) -> list[list[int]]:
             list_of_reports.append(report)
     return list_of_reports
 
+
 def safety_test_1(deltas: list[int]) -> int:
     return abs(sum(x > 0 for x in deltas) - sum(x < 0 for x in deltas))
+
 
 def safety_test_2(deltas: list[int]) -> int:
     delta_limit = 4
     deltas_above_limit: list[int] = [abs(num) >= delta_limit for num in deltas]
     return sum(deltas_above_limit)
 
+
 def is_safe(report: list[int]) -> bool:
-    deltas: list[int] = [b-a for a,b in itertools.pairwise(report)]
+    deltas: list[int] = [b - a for a, b in itertools.pairwise(report)]
     pass_test_1: bool = safety_test_1(deltas) >= len(deltas)
     pass_test_2: bool = safety_test_2(deltas) == 0
     return pass_test_1 and pass_test_2
+
 
 def day_2_part_1(reports: list[list[int]]) -> int:
     safe_reports: list[bool] = []
@@ -32,6 +35,7 @@ def day_2_part_1(reports: list[list[int]]) -> int:
         safe_reports.append(safety)
     return sum(safe_reports)
 
+
 def day_2_part_2(reports: list[list[int]]) -> int:
     safe_reports: list[bool] = []
     for report in reports:
@@ -39,13 +43,14 @@ def day_2_part_2(reports: list[list[int]]) -> int:
             safe_reports.append(True)
         else:
             for i in range(len(report)):
-                modified_report = report[:i] + report[i+1:]
+                modified_report = report[:i] + report[i + 1 :]
                 if is_safe(modified_report):
                     safe_reports.append(True)
                     break
             else:
                 safe_reports.append(False)
     return sum(safe_reports)
+
 
 file_path: Path = Path.cwd() / "Input" / "day2.txt"
 reports: list[list[int]] = read_input(file_path)
