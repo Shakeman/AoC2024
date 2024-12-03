d40e5129a085bc26c45f19fb2ebc71416e7a680e
John Doe <johndoe@example.com>
2024-12-03 06:39:01 -0700

    Passes day 2

--- Day/day2.py
+++ Day/day2.py
@@ -1,5 +1,6 @@
 import itertools
 from pathlib import Path
+import re
 
 
 def read_input(file_path: Path) -> list[list[int]]:
@@ -10,21 +11,43 @@ def read_input(file_path: Path) -> list[list[int]]:
             list_of_reports.append(report)
     return list_of_reports
 
-def safety_test_1(deltas: list[int]) -> bool:
-    return all(x > 0 for x in deltas) or all(x < 0 for x in deltas)
+def safety_test_1(deltas: list[int]) -> int:
+    return abs(sum(x > 0 for x in deltas) - sum(x < 0 for x in deltas))
 
-def safety_test_2(deltas: list[int]) -> bool:
-    absolute_deltas = [abs(num) for num in deltas]
-    return max(absolute_deltas) < 4  # noqa: PLR2004
+def safety_test_2(deltas: list[int]) -> int:
+    delta_limit = 4
+    deltas_above_limit: list[int] = [abs(num) >= delta_limit for num in deltas]
+    return sum(deltas_above_limit)
+
+def is_safe(report: list[int]) -> bool:
+    deltas: list[int] = [b-a for a,b in itertools.pairwise(report)]
+    pass_test_1: bool = safety_test_1(deltas) >= len(deltas)
+    pass_test_2: bool = safety_test_2(deltas) == 0
+    return pass_test_1 and pass_test_2
 
 def day_2_part_1(reports: list[list[int]]) -> int:
     safe_reports: list[bool] = []
     for report in reports:
-        deltas: list[int] = [b-a for a,b in itertools.pairwise(report)]
-        pass_test_1: bool = safety_test_1(deltas)
-        pass_test_2: bool = safety_test_2(deltas)
-        safe_reports.append(pass_test_1 and pass_test_2)
+        safety: bool = is_safe(report)
+        safe_reports.append(safety)
     return sum(safe_reports)
+
+def day_2_part_2(reports: list[list[int]]) -> int:
+    safe_reports: list[bool] = []
+    for report in reports:
+        if is_safe(report):
+            safe_reports.append(True)
+        else:
+            for i in range(len(report)):
+                modified_report = report[:i] + report[i+1:]
+                if is_safe(modified_report):
+                    safe_reports.append(True)
+                    break
+            else:
+                safe_reports.append(False)
+    return sum(safe_reports)
+
 file_path: Path = Path.cwd() / "Input" / "day2.txt"
 reports: list[list[int]] = read_input(file_path)
 print(day_2_part_1(reports))
+print(day_2_part_2(reports))
--- Test/test_day2.py
+++ Test/test_day2.py
@@ -1,8 +1,11 @@
-from Day.day2 import day_2_part_1
+from Day.day2 import day_2_part_1, day_2_part_2
 
 
 def test_day2_part1() -> None:
     test_reports: list[list[int]] = [[7, 6, 4, 2, 1], [1, 2, 7, 8, 9], [9, 7, 6, 2, 1], [1, 3, 2, 4, 5], [8, 6, 4, 4, 1], [1, 3, 6, 7, 9]]
     assert day_2_part_1(test_reports) == 2
 
+def test_day2_part2() -> None:
+    test_reports: list[list[int]] = [[7, 6, 4, 2, 1], [1, 2, 7, 8, 9], [9, 7, 6, 2, 1], [1, 3, 2, 4, 5], [8, 6, 4, 4, 1], [1, 3, 6, 7, 9]]
+    assert day_2_part_2(test_reports) == 4
 
