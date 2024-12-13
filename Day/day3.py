4e2c2a9514468cedc467d40ca355a23f0a78fe8e
John Doe <johndoe@example.com>
2024-12-13 07:30:18 -0700

    Passes ruff and pyright

--- Day/day3.py
+++ Day/day3.py
@@ -1,6 +1,6 @@
 import re
 from pathlib import Path
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Literal
 
 if TYPE_CHECKING:
     from collections.abc import Iterator
@@ -40,11 +40,11 @@ def find_and_multiply_and_do(input_string: str) -> int:
     enabled = True
 
     # Create a list of all instructions with their positions
-    instructions = (
+    instructions: list[tuple[int, Literal["mul"], tuple[str, str]] | tuple[int, str]] = (
         [(match.start(), "do") for match in do_matches]
         + [(match.start(), "don't") for match in dont_matches]
         + [(match.start(), "mul", match.groups()) for match in mul_matches]
-    )
+    )  # type: ignore[AssignmentType]
 
     # Sort instructions by their positions
     instructions.sort()
@@ -56,7 +56,7 @@ def find_and_multiply_and_do(input_string: str) -> int:
         elif instruction[1] == "don't":
             enabled = False
         elif instruction[1] == "mul" and enabled:
-            x, y = map(int, instruction[2])
+            x, y = map(int, instruction[2])  # type: ignore[GeneralTypeIssues]
             total_sum += x * y
 
     return total_sum
